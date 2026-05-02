711fa8f121413fb758af1f72534b55916b908bb1
rhmuja <r.h.muja@gmail.com>
2026-05-02 21:18:15 +0600

    Create text_middle_challange.py
    PYTHON need to put center of the Squre

---
+++ Basics/text_middle_challange.py
@@ -0,0 +1,23 @@
+#The question is "Can you print the word 'PYTHON' or your own name exactly in the center of the box you just created?"
+
+"Solution"
+
+#border_line
+print("* " * 13)
+
+#middle_line
+print("*" , "\t\t\t*", sep="")
+print("*" , "\t\t\t*", sep="")
+print("*" , "\t\t\t*", sep="")
+
+#print_python or name
+print("*" , "\t Python \t*", sep="")
+
+#middle_line
+print("*" , "\t\t\t*", sep="")
+print("*" , "\t\t\t*", sep="")
+print("*" , "\t\t\t*", sep="")
+
+
+#border_line
+print("* " * 13)
