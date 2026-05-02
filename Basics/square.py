259f6501166bb90af8b6ccab8ff155eb7f047c26
rhmuja <r.h.muja@gmail.com>
2026-05-02 20:49:49 +0600

    Create square.py
    make square with syntax,statement and output

---
+++ Basics/square.py
@@ -0,0 +1,21 @@
+#border_line
+print("* " * 17)
+#middle_line
+#here t using for making spaces and \t = 4 spaace
+print("*", "\t\t\t\t*")
+print("*", "\t\t\t\t*")
+print("*", "\t\t\t\t*")
+print("*", "\t\t\t\t*")
+print("*", "\t\t\t\t*")
+print("*", "\t\t\t\t*")
+print("*", "\t\t\t\t*")
+print("*", "\t\t\t\t*")
+print("*", "\t\t\t\t*")
+print("*", "\t\t\t\t*")
+print("*", "\t\t\t\t*")
+print("*", "\t\t\t\t*")
+print("*", "\t\t\t\t*")
+print("*", "\t\t\t\t*")
+print("*", "\t\t\t\t*")
+#border_line
+print("* " * 17)
