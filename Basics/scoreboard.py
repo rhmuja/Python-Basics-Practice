ae0da05632b98723cdf39c938bdcec3ded04ca76
rhmuja <r.h.muja@gmail.com>
2026-05-02 21:25:58 +0600

    Create scoreboard.py
    syntax, and output used

---
+++ Basics/scoreboard.py
@@ -0,0 +1,14 @@
+#The question is "Can you print a "Scoreboard" that shows the result of $500 + 250$ and $1000 - 450$ using only print?"
+
+"""Solution"""
+
+#print "Scoreboard"
+
+print("SCOREBOARD")
+
+#print_line
+print("-" * 10)
+
+#show_result
+print("Team A: ", 500 + 250)
+print("Team B: ", 1000 - 450)
