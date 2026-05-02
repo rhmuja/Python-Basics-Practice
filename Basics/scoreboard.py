7bd7e5230286402426292f7e667a9c428ed03f06
rhmuja <r.h.muja@gmail.com>
2026-05-02 21:26:50 +0600

    Update scoreboard.py
    make scoreboard

--- Basics/scoreboard.py
+++ Basics/scoreboard.py
@@ -1,5 +1,14 @@
 #The question is "Can you print a "Scoreboard" that shows the result of $500 + 250$ and $1000 - 450$ using only print?"
 
+"Need to print -
+
+SCOREBOARD
+----------
+Team A: 750
+Team B: 550
+
+"
+
 """Solution"""
 
 #print "Scoreboard"
